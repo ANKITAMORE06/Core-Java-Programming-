b1fc5d5bcd98d4e957642ca95bbdccea1aadc7ce
Ankita More <154222994+ANKITAMORE06@users.noreply.github.com>
2025-08-19 22:41:40 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,18 @@
+class Age 
+{
+  public static void main(String [] args)
+  {
+   byte childAge = 10;
+   byte teenAge = 20;
+   byte Senior_citizen = 64;
+   byte votingID = 18;
+   
+   System.out.println("Age Category Information");
+   System.out.println("CHILD :"+childAge);
+   System.out.println("Youngstar :"+teenAge);
+   System.out.println("Senior Citizen :"+Senior_citizen);
+   System.out.println("VotingID :"+votingID);
+
+  }
+ 
+}
---
+++ Car.java
@@ -0,0 +1,21 @@
+class Car
+{
+   public static void main(String [] args)
+   {
+     String carName = "OMINI";
+     byte seatingCapacity = 8;
+     byte tyres = 5;
+     byte gears = 6;
+     byte doors = 5;
+     byte mirrors = 3;
+     
+     System.out.println("CAR DETAILS");
+
+     System.out.println("carName : "+carName);
+     System.out.println("seating Capacity : "+seatingCapacity);
+     System.out.println("Tyres  : "+tyres);
+     System.out.println("Geras : "+gears);
+     System.out.println("Doors : "+doors);
+     System.out.println("Mirrors : "+mirrors);
+   }
+}
